bd6f8254be0672d59e26ae36010b58132d60aa87
WNjihia <dorisn172@gmail.com>
2024-03-21 22:46:57 +0300

    add toggle WIP

--- assets/js/main.js
+++ assets/js/main.js
@@ -224,7 +224,27 @@
 }(window, document, window.jQuery);
 
 // Dark and Light mode toggle
-function darkMode () {
-    var element = document.body;
-    element.classList.toggle("dark-mode")
+
+const toggleSwitch = document.getElementById("checkbox");
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
 }
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
